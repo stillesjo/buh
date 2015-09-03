f8f87a822c9e87203ba53c1aeb80cee90b8e0fac
Alexander Stillesjö <a.stillesjo@gmail.com>
2015-09-04 00:46:35 +0200

    Will now search and print github full_name when searching

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 #!/usr/bin/env node
 'use strict';
 
-var pack = require('./package');
-
-var Liftoff = require('liftoff');
 var argv = require('minimist')(process.argv.slice(2));
 var mout = require('mout');
+var config = require('./lib/config');
 var commands = require('./lib/commands');
+var request = require('request');
 
 process.env.INIT_CWD = process.cwd();
 
 
 var run = function() {
-  //if (version)
-  //  console.log("Running version " + pack.version);
-  if (argv._ && argv._.length >0) {
+  if (argv._ && argv._.length > 0) {
     var commandName = argv._.shift();
-    mout.object.get(commands, commandName)(argv._);
-  } 
+    mout.object.get(commands, commandName)(config, request, argv._);
+  }
 };
 module.exports = run;
