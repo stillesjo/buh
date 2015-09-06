a367640de5d99f61d175f90f0690c86854d0deda
Alexander Stillesjö <a.stillesjo@gmail.com>
2015-09-06 23:06:56 +0200

    Minor improvements

--- index.js
+++ index.js
@@ -10,16 +10,21 @@ var commands = require('./lib/commands');
 
 process.env.INIT_CWD = process.cwd();
 
+function help() {
+  mout.object.get(commands, 'help')();
+}
 
 var run = function() {
   if (argv._ && argv._.length > 0) {
     var commandName = argv._.shift();
     if (mout.object.has(commands, commandName)) {
       mout.object.get(commands, commandName)(config, request, argv._);
+    } else {
+      help();
     }
   } else { // No command was specified
     // Print help info
-    mout.object.get(commands, 'help')();
+    help();
   }
 };
 module.exports = run;
