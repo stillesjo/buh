d47bbb904ed57a02617eeedbaf70c1f25acd2db7
Alexander Stillesjö <a.stillesjo@gmail.com>
2015-09-21 18:52:58 +0200

    Logging + install.

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ var commands = require('./lib/commands');
 process.env.INIT_CWD = process.cwd();
 
 
-function help(logger) {
-  mout.object.get(commands, 'help')(logger);
+function help(out) {
+  mout.object.get(commands, 'help')(out);
 }
 
-var run = function(logger, api) {
+var run = function(out, api) {
   if (argv._ && argv._.length > 0) {
     var commandName = argv._.shift();
     if (mout.object.has(commands, commandName)) {
-      mout.object.get(commands, commandName)(api, argv, logger);
+      mout.object.get(commands, commandName)(api, argv, out);
     } else {
-      help(logger);
+      help(out);
     }
   } else { // No command was specified
     // Print help info
-    help(logger);
+    help(out);
   }
 };
 module.exports = run;
--- test/install.spec.js
+++ test/install.spec.js
@@ -4,12 +4,23 @@ var expect = require('expect.js');
 var install = require('../lib/commands/install');
 var helper = require('./testhelper');
 var installInputHelp = {help: true};
+var installGithubRepository = {_: ['stillesjo/buh']};
+
+var throwingApi = { clone: function() {
+  throw 'TESTEXCEPTION';
+},};
 
 describe('install', function() {
+  describe('feature', function() {
+    it ('should run api method', function() {
+      expect(install).withArgs(throwingApi,
+        installGithubRepository, undefined)
+        .to.throwException(/TESTEXCEPTION/);
+    });
+  });
   describe('install help', function() {
     it('should return helptext', function() {
       install(null, installInputHelp, function(output) {
-        console.log(output);
         expect(output).to.not.be(undefined);
         expect(output).to.be.a('string');
         expect(output).to.equal(helper.getInstallHelp());
