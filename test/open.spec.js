1f8ca82a0a683228bce38b67035e335779765d46
Alexander Stillesjö <a.stillesjo@gmail.com>
2015-09-21 22:03:18 +0200

    Basic tests for open.

--- test/open.spec.js
+++ test/open.spec.js
@@ -5,6 +5,7 @@ describe('open', function() {
   it('should exist as command in commands', function() {
     var commands = require('../lib/commands');
     expect(commands).to.have.property('open');
+    expect(commands.open).to.be.a('function');
   });
 
   describe('feature', function() {
