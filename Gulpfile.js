a1b464eb14d50a31b8d59470a4d4326df89e8ae7
Alexander Stillesjö <a.stillesjo@gmail.com>
2015-09-21 22:46:55 +0200

    Simplyfies gulpfile with smarter, non-crashing watches
    Removes development dependencies no longer needed

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,6 @@
 'use strict';
 
 var gulp = require('gulp');
-var watch = require('gulp-watch');
-var batch = require('gulp-batch');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var mocha = require('gulp-mocha');
@@ -34,7 +32,7 @@ gulp.task('jscs', function() {
   .on('error', ErrorHandler);
 });
 
-gulp.task('exec-test', function() {
+gulp.task('test', function() {
   gulp.src(testfiles)
   .pipe(cover.instrument({
     pattern: sourceFiles,
@@ -47,25 +45,11 @@ gulp.task('exec-test', function() {
 });
 
 
-gulp.task('test', ['exec-test', 'lint', 'jscs']);
-gulp.task('watch', ['test'], function() {
-  watch('**/*.js', batch(function(events, done) {
-    gulp.start('test', done)
-    .on('error', ErrorHandler);
-  }));
-});
-gulp.task('watch-no-lints', ['exec-test'], function() {
-  watch(jsfiles, batch(function(events, done) {
-    gulp.start('exec-test', done)
-    .on('error', ErrorHandler);
-  }));
+gulp.task('watch', ['test', 'lint', 'jscs'], function() {
+  gulp.watch('**/*.js', ['test', 'lint', 'jscs'])
+  .on('error', function(event){
+    console.log(event);
+  });
 });
 
 gulp.task('default', ['watch']);
-
-gulp.task('testing', ['jscs'],function() {
-  watch(jsfiles, batch(function(events, done) {
-    gulp.start('jscs', done)
-    .on('error', ErrorHandler);
-  }));
-});
